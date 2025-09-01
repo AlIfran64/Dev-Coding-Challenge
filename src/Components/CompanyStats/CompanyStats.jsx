b96e29e81ceb8310badb77637552e2243ae9f732
Al Ifran <ifran15-2915@diu.edu.bd>
2025-09-01 20:51:05 +0600

    company stats completed

--- src/Components/CompanyStats/CompanyStats.jsx
+++ src/Components/CompanyStats/CompanyStats.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import boltshift from '../../assets/boltshift.png';
+import featherdev from '../../assets/featherdev.png';
+import globalbank from '../../assets/global bank.png';
+import lightbox from '../../assets/lightbox.png';
+import nietzsche from '../../assets/Nietzsche.png';
+import spherule from '../../assets/spherule.png';
 
 const CompanyStats = () => {
   return (
@@ -6,28 +12,36 @@ const CompanyStats = () => {
       <h1 className='text-center text-base text-[#475467]'>Join 4,000+ companies already growing</h1>
 
       {/* Company's */}
-      <div>
+      <div className='grid grid-cols-6 gap-8 place-items-center mt-8'>
         {/* Company: Boltshift */}
         <div>
-          <img src={boltshift} alt="company: Boltshift" />
+          <img src={boltshift} alt="Boltshift logo" />
         </div>
 
         {/* Company: Lightbox */}
         <div>
-          <img src={lightbox} alt="company: Lightbox" />
+          <img src={lightbox} alt="Lightbox logo" />
         </div>
 
         {/* Company: FeatherDev */}
-        <div></div>
+        <div>
+          <img src={featherdev} alt="FeatherDev logo" />
+        </div>
 
         {/* Company: Spherule */}
-        <div></div>
+        <div>
+          <img src={spherule} alt="Spherule logo" />
+        </div>
 
         {/* Company: GlobalBank */}
-        <div></div>
+        <div>
+          <img src={globalbank} alt="GlobalBank logo" />
+        </div>
 
         {/* Company: Nietzsche */}
-        <div></div>
+        <div>
+          <img src={nietzsche} alt="Nietzsche logo" />
+        </div>
       </div>
     </div>
   );
